68bd3f41816a7fa7ac5df471c09f88c624dab07e
ibnatysalsabila <salsajpr01@gmail.com>
2026-03-13 05:53:52 +0700

    Initial commit

---
+++ src/main/java/com/deploy/tugascrud/ktp/Application.java
@@ -0,0 +1,13 @@
+package com.deploy.tugascrud.ktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
